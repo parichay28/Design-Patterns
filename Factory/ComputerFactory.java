369ebaba2bbdf95cd4f84dfce2076978709fa58c
qainfotech <parichayagrawal@qainfotech.com>
2019-01-31 13:25:08 +0530

    test

--- Factory/ComputerFactory.java
+++ Factory/ComputerFactory.java
@@ -1,14 +1,15 @@
 public class ComputerFactory {
-    public Computer getType(String typeComputer){
-        if (typeComputer == null){
-            return null;
-        }
-        else if(typeComputer.equalsIgnoreCase("PC")){
+    public static Computer getComputer(String typeComputer){
+        
+         if(typeComputer.equalsIgnoreCase("PC")){
             return new PC();
         }
         else if(typeComputer.equalsIgnoreCase("Server")){
             return new Server();
         }
-        return null;
+
+        else{
+            return null;
+        }
     }
 }
